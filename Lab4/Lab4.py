0258aa6e8f06998ebfd2f53d474975ed00cb41c0
Alex <alexanderfboyceweb3@gmail.com>
2025-05-07 20:23:15 -0700

    parts 1 and 2 done

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -25,17 +25,76 @@ geoData = geoData.to_crs("EPSG:3395")
 print("Set crs to:", geoData.crs)
 
 #exclude non-contiguous states
-excluded_states = {'AK', 'HI', 'PR', 'GU', 'VI', 'MP', 'AS'}
+excluded_states = {'AK', 'HI', 'PR', 'GU', 'VI', 'MP', 'AS','DC'}
 
 #filter using this set
 geoData = geoData[~geoData['STUSPS'].isin(excluded_states)]
 
 #dissolve to create state boundaries
-states = geoData.dissolve(by='STUSPS')
+states = geoData.dissolve(by='STUSPS', as_index=False, aggfunc='first')
 
-# Plot the map
-states.plot(edgecolor='black', figsize=(12, 8))
-plt.title("Contiguous United States (from County Subdivisions)")
-plt.axis("off")
-plt.show()
+
+#Visualize the Map of the United States
+def baseMap():
+    fig, ax = plt.subplots(figsize=(8, 5))
+    states.plot(ax=ax, edgecolor='black', cmap='Pastel1')
+    ax.set_title("Base Map of 48 Contiguous United States", fontsize=20)
+
+    #turn off axis and tighten layout
+    ax.axis('off')
+    plt.tight_layout()
+
+    # Save before show
+    fig.savefig('Boyce_baseMap.png')
+    plt.show()
+
+    print("baseMap Generated")
+
+
+
+#Visualize the Distribution of geographic data
+def geoMap():
+    fig = plt.figure(1, figsize=(20, 10))
+    ax = fig.add_subplot()
+    ax.set_title("Map of 48 Contiguous United States with Geographic Data", fontsize=20)
+
+    #plot the state names
+    states.apply(lambda x: ax.annotate(x.STUSPS, xy=x.geometry.centroid.coords[0], ha='center', fontsize=14), axis=1)
+
+    #plot the state boundaries
+    states.boundary.plot(ax=ax, color='Red', linewidth=.6)
+
+    #plot the states
+    states.plot(ax=ax, cmap='Pastel2')
+
+    #turn off axis and tighten layout
+    ax.axis('off')
+    plt.tight_layout()
+
+    #legend text box
+    key_text = "\n".join([f"{abbr} — {name}" for abbr, name in zip(states['STUSPS'], states['STATE_NAME'])])
+
+    #debug
+    print(key_text)
+
+    props = dict(boxstyle='round', facecolor='white', alpha=0.8)
+    ax.text(0.95, 0.5, key_text, transform=ax.transAxes, fontsize=12, verticalalignment='center', bbox=props, family='monospace')
+
+    plt.show()
+    fig.savefig('Boyce_geoMap.png')     # save the map as a png file
+    print("geoMap Generated")
+
+'''Generate Five Representative and Unique Statistical Results:
+Select any five meaningful statistics or insights from the dataset. These could be:
+The total number of tornadoes per state.
+Any other insightful measure derived from the data.
+Present these statistical results through appropriate visualizations (e.g., bar charts,
+line graphs, histograms, or box plots).
+Ensure that each visualization is clearly labeled and includes an interpretation of
+the results'''
+
+
+baseMap() #Visualize the Map of the United States
+geoMap()  #Visualize the Distribution of geographic data
+
 
